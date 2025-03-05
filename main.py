d0d5c59581ae1bca1cb36b1692658bbad3c19dc7
Shv3pcy <153019248+Shv3pcy@users.noreply.github.com>
2025-03-06 00:02:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+from handlers import router
+
+
+async def main():
+  bot = Bot(token="тут токен от бота @SysBmi_bot") 
+  dp = Dispatcher() # обработчик команд
+  dp.include_router(router)
+  await dp.start_polling(bot)
+  print('BOT ON (ВКЛ)')
+
+if __name__ == '__main__':
+  try:
+      asyncio.run(main())
+  except KeyboardInterrupt:
+      print('BOT OFF (ВЫКЛ)')
