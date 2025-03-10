be533a5757287ccd71676b1cd7ba93bcfaf1c461
N3rcy <nercysvoboda@gmail.com>
2025-03-10 19:08:38 +0200

    black formatting

--- calculate.py
+++ calculate.py
@@ -4,15 +4,18 @@
 t.me/SysBmi_Bot
 """
 
-def bmi_calc(body_weight, body_height):
-         body_height = float(body_height)
-         result = body_weight / (body_height ** 2)
-         result = round(result, 2)
-         return result
 
-#x = input()
-#y = input()
-#z = bmi_calc(x, y)
+def bmi_calc(body_weight, body_height):
+    body_height = float(body_height)
+    result = body_weight / (body_height**2)
+    result = round(result, 2)
+    return result
+
+
+# x = input()
+# y = input()
+# z = bmi_calc(x, y)
+
 
 def sys2_10(number):
     number = str(number)
@@ -20,12 +23,12 @@ def sys2_10(number):
     return result
 
 
-
 def sys10_2(number):
     number = int(number)
     result = bin(number)
     return result
 
-#x  = sys2_10('1001')
-#y = sys10_2('9')
-#z = f'{x}\n{y}'
+
+# x  = sys2_10('1001')
+# y = sys10_2('9')
+# z = f'{x}\n{y}'
